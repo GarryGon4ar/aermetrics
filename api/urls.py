47e5175803b3005b976e363fda9a7d9790ab3afc
Dzhumabaev Erlan <garrygon4ar@gmail.com>
2022-06-06 00:49:52 +0600

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,9 @@
 # api/urls.py
 from django.urls import path
 
-from api.views import LogsAPIView, LogsFileUploadView, AircraftsListView, StatusesListView, TypesListView, MyGenericView
+from api.views import LogsAPIView, LogsFileUploadView
 
 urlpatterns = [
     path('', LogsAPIView.as_view()),
     path('upload/', LogsFileUploadView.as_view(), name='upload-file'),
-    path('aircrafts/', AircraftsListView.as_view(), name='aircrafts'),
-    path('statuses/', StatusesListView.as_view(), name='statuses'),
-    path('types/', TypesListView.as_view(), name='statuses'),
-    path('statistics/', MyGenericView.as_view(), name='statuses'),
 ]
